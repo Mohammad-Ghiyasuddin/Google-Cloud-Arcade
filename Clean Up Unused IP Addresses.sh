5368a0292737aef8618d68d985a2a0c0fd53bacb
Mohammad Ghiyasuddin <103780000+Mohammad-Ghiyasuddin@users.noreply.github.com>
2023-12-09 16:29:56 +0530

    Update Clean Up Unused IP Addresses.sh

--- Clean Up Unused IP Addresses.sh
+++ Clean Up Unused IP Addresses.sh
@@ -20,29 +20,29 @@ cd $WORKDIR/unused-ip
 export USED_IP=used-ip-address
 export UNUSED_IP=unused-ip-address
 
-gcloud compute addresses create $USED_IP --project=$PROJECT_ID --region=us-central1
-gcloud compute addresses create $UNUSED_IP --project=$PROJECT_ID --region=us-central1
+gcloud compute addresses create $USED_IP --project=$PROJECT_ID --region="Enter Your Region"
+gcloud compute addresses create $UNUSED_IP --project=$PROJECT_ID --region="Enter Your Region"
 
-gcloud compute addresses list --filter="region:(us-central1)"
-
-
+gcloud compute addresses list --filter="region:('Enter Your Region')"
 
 
-export USED_IP_ADDRESS=$(gcloud compute addresses describe $USED_IP --region=us-central1 --format=json | jq -r '.address')
+
+
+export USED_IP_ADDRESS=$(gcloud compute addresses describe $USED_IP --region='Enter Your Region' --format=json | jq -r '.address')
 
 
 
 Task 3
 
 gcloud compute instances create static-ip-instance \
---zone=us-central1-f \
+--zone="Enter your zone" \
 --machine-type=e2-medium \
 --subnet=default \
 --address=$USED_IP_ADDRESS
 
 
 
-gcloud compute addresses list --filter="region:(us-central1)"
+gcloud compute addresses list --filter="region:('Enter Your Region')"
 
 
 Task 4
@@ -53,7 +53,7 @@ cat $WORKDIR/unused-ip/function.js | grep "const compute" -A 31
 
 Task 5
 
-gcloud functions deploy unused_ip_function --trigger-http --runtime=nodejs12 --region=us-central1
+gcloud functions deploy unused_ip_function --trigger-http --runtime=nodejs12 --region='Enter Your Region'
 
 
 Type 'Y' if prompted
@@ -61,7 +61,7 @@ Type 'Y' if prompted
 
 
 
-export FUNCTION_URL=$(gcloud functions describe unused_ip_function --region=us-central1 --format=json | jq -r '.httpsTrigger.url')
+export FUNCTION_URL=$(gcloud functions describe unused_ip_function --region="Enter Your Region" --format=json | jq -r '.httpsTrigger.url')
 
 
 
@@ -70,20 +70,20 @@ export FUNCTION_URL=$(gcloud functions describe unused_ip_function --region=us-c
 Task 6
 
 
-gcloud app create --region us-central
+gcloud app create --region "Enter Your Region"
 
 gcloud scheduler jobs create http unused-ip-job \
 --schedule="* 2 * * *" \
 --uri=$FUNCTION_URL \
---location=us-central1
+--location="Enter Your Region"
 
 
 
 gcloud scheduler jobs run unused-ip-job \
---location=us-central1
+--location="Enter Your Region"
 
 
-gcloud compute addresses list --filter="region:(us-central1)"
+gcloud compute addresses list --filter="region:("Enter Your Region")"
 
 
 
